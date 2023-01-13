0caa005820613a948efbdfb081c253022961571c
ExASN <expedito.avelino@gmail.com>
2023-01-13 16:09:58 -0300

    Base da aplicação criada

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    cout << "Ola humano"<< endl << "Ola animal" << endl;
+    return 0;
+}
