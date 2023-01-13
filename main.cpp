6635b6775fd2c6fb2f1db5f54f0e426fdfa9f02d
ExASN <expedito.avelino@gmail.com>
2023-01-13 16:24:03 -0300

    código funcional

--- main.cpp
+++ main.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 int main()
 {
-    cout << "Ola humano"<< endl << "Ola animal" << endl;
+    queue <string> numbers;
+    numbers.push("zero");
+    numbers.push("one");
+    numbers.push("two");
+    numbers.push("three");
+    numbers.push("four");
+    numbers.push("five");
+    numbers.push("six");
+    numbers.push("seven");
+    numbers.push("eigth");
+    numbers.push("nine");
+
+   cout << "The size of queue is: " << numbers.size() << endl;
+   cout << "The number in front of the queue is: " << numbers.front() << endl;
+   cout << "The number behind the queue is: " << numbers.back() << endl;
+
     return 0;
 }
